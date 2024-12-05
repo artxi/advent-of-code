2ccdf72d7f2818d2aff6f9162e3efad7dedc2a58
Artzai <aartxi@gmail.com>
2024-12-05 02:19:18 +0100

    feat: Get file from aoc web

--- controllers/day.js
+++ controllers/day.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const fetch = require('node-fetch');
+
+const settings = require('../config/settings')
 
 function initDay(year, day) {
     const basePath = path.resolve(__dirname, '../');
@@ -14,40 +17,66 @@ function initDay(year, day) {
 function initFileSystem(basePath, year, day) {
     const template = fs.readFileSync(path.resolve(basePath, 'config/dayTemplate.js'), 'utf-8');
 
+    day = (day.length < 2 ? '0' : '') + day;
     const yearPath = path.resolve(basePath, year);
-    const dayPath = path.resolve(yearPath, (day.length < 2 ? '0' : '') + day);
+    const dayPath = path.resolve(yearPath, day);
 
     if (!fs.existsSync(yearPath)) {
         fs.mkdirSync(yearPath);
+        console.log(`   Created folder ${year}`);
     }
 
     if (!fs.existsSync(dayPath)) {
         fs.mkdirSync(dayPath);
-    }
+        console.log(`   Created folder ${day}`);
 
-    fs.writeFileSync(path.resolve(dayPath, 'part1.js'), template);
-    fs.writeFileSync(path.resolve(dayPath, 'part2.js'), template);
+        fs.writeFileSync(path.resolve(dayPath, 'part1.js'), template);
+        fs.writeFileSync(path.resolve(dayPath, 'part2.js'), template);
+
+        console.log(`   Created part templates`);
+    }
 
     return dayPath;
 }
 
-async function getInput(path, year, day) {
-    if (existsInputFile(path)) {
+async function getInput(dayPath, year, day) {
+    if (existsInputFile(dayPath)) {
         return;
     }
 
-    // TO-DO: Get input file
-    const data = 'kk';
-
-    createInputFile(path, data);
-}
-
-function createInputFile(filePath, data) {
-    fs.writeFileSync(path.resolve(filePath, 'input.txt'), data);
+    await getInputData(dayPath, year, day);
 }
 
 function existsInputFile(basePath) {
     return fs.existsSync(path.resolve(basePath, 'input.txt'));
+}
+
+async function getInputData(filePath, year, day) {
+    const inputFileName = path.resolve(filePath, 'input.txt');
+
+    let fileStream = fs.createWriteStream(inputFileName);
+
+    await fetch(`https://adventofcode.com/${year}/day/${day}/input`, {
+        method: 'GET',
+        headers: {
+            'Cookie': settings.cookie,
+        }
+    })
+    .then(
+        res =>
+        new Promise((resolve, reject) => {
+            res.body.pipe(fileStream);
+            res.body.on("end", () => {
+                fs.readFile(inputFileName, 'utf-8', (err, data) => {
+                    if(err) {
+                        return reject(err);
+                    }
+                    console.log(`   Created input file`);
+                });
+            });
+            fileStream.on("error", reject);
+        })
+    );
 }
 
 module.exports = {

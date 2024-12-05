525c3926aaf3a780e1729bbf7eb82fa26a3a86ae
Artzai <aartxi@gmail.com>
2024-12-05 23:23:13 +0100

    feat: Read lines in template

--- 2024/05/part2.js
+++ 2024/05/part2.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 const input = fs.readFileSync(path.resolve(__dirname, 'input.txt')).toString();
-
 const lines = input.split('\n');
 
 let rules = lines.filter(l => l.match(/\|/));
--- config/dayTemplate.js
+++ config/dayTemplate.js
@@ -2,3 +2,4 @@ const fs = require('fs');
 const path = require('path');
 
 const input = fs.readFileSync(path.resolve(__dirname, 'input.txt')).toString();
+let lines = input.split('\n').filter(l => l);

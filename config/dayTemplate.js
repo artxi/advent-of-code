1c7a4bfbad0c71e6a4e3751bbb02dc16020830ab
Artzai <aartxi@gmail.com>
2024-12-06 17:48:06 +0100

    feat: Time execs on day template

--- config/dayTemplate.js
+++ config/dayTemplate.js
@@ -1,5 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
+console.time('Time');
+
 const input = fs.readFileSync(path.resolve(__dirname, 'input.txt')).toString();
 const lines = input.split('\n').filter(l => l);
+
+let answer;
+
+//
+
+console.log(`Answer: ${answer}`);
+console.timeEnd('Time');
